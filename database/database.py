36710651be7cf50d26819bafacdb76eaac5101e2
adriansangadi <73935142+adriansangadi@users.noreply.github.com>
2024-02-20 02:21:09 +0700

    Add files via upload

---
+++ database/database.py
@@ -0,0 +1,31 @@
+#(©)CodeXBotz
+
+
+
+
+import pymongo, os
+from config import DB_URI, DB_NAME
+
+
+dbclient = pymongo.MongoClient(DB_URI)
+database = dbclient[DB_NAME]
+
+
+user_data = database['users']
+
+
+
+async def present_user(user_id : int):
+    return bool(found := user_data.find_one({'_id': user_id}))
+
+async def add_user(user_id: int):
+    user_data.insert_one({'_id': user_id})
+    return
+
+async def full_userbase():
+    user_docs = user_data.find()
+    return [doc['_id'] for doc in user_docs]
+
+async def del_user(user_id: int):
+    user_data.delete_one({'_id': user_id})
+    return
